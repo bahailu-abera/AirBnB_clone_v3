a82fd9fd420e3788c46e585fa650361b0e03f05c
bahailu <aberabahailu4@gmail.com>
2022-10-23 22:02:08 +0300

    cors enabled

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ Application logic
 from os import getenv
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
@@ -12,6 +13,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+CORS(app, origins="0.0.0.0")
 api_host = getenv('HBNB_API_HOST', '0.0.0.0')
 api_port = getenv('HBNB_API_PORT', '5000')
 
